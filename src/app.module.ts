17f5d5d16c446a35395b7524d1740a88eaeab6d5
Henrique Ferreira Silva <henrique_silva_ferreira@hotmail.com>
2024-04-12 14:42:16 -0300

    some adjusts

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,8 @@ import * as redisStore from 'cache-manager-redis-store';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
-      port: 5432,
+      // port: 5432,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,24 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { RedisOptions, TcpClientOptions, Transport } from '@nestjs/microservices';
-import { HealthCheck, HealthCheckService, MicroserviceHealthIndicator } from '@nestjs/terminus';
+import { HealthCheck, HealthCheckService, MicroserviceHealthIndicator, TypeOrmHealthIndicator } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
     constructor(
         private health: HealthCheckService,
         private microservice: MicroserviceHealthIndicator,
+        private db: TypeOrmHealthIndicator
       ) {}
 
     @Get()
     @HealthCheck()
     check() {
         return this.health.check([
-        async () =>
+          async () =>
             this.microservice.pingCheck<TcpClientOptions>('tcp', {
-            transport: Transport.TCP,
-            options: { host: 'localhost', port: 3001 },
-        }),
-        async () =>
+              transport: Transport.TCP,
+              options: { host: 'localhost', port: 3001 },
+            }),
+          async () =>
             this.microservice.pingCheck<RedisOptions>('redis', {
               transport: Transport.REDIS,
               options: {
@@ -26,6 +27,7 @@ export class HealthController {
                 port: 6379,
               },
             }),
+          async () => this.db.pingCheck('database'),
         ]);
       }
 }
